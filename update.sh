b52ffaf8ac0ec7e76a554b3ddfe951775061646e
Eleni Bantzi <bantzi.eleni@gmail.com>
2025-04-09 22:17:31 +1200

    add another user

--- update.sh
+++ update.sh
@@ -3,12 +3,12 @@
 set -e  # Exit immediately on error
 
 echo "Stopping and removing containers..."
-docker-compose down
+docker-compose down -v
 
-echo "Pulling latest Docker images..."
-docker-compose pull
+docker rmi -f fuel50_flyway
+docker rmi -f postgres
 
 echo "Starting containers in detached mode..."
-docker-compose up -d
+docker-compose up --build -d
 
 echo "Update complete. Latest images are running."
